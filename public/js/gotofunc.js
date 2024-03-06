11a7f0a11356b5afc7a35ee468a1f49c2b99fcf2
poomport <131750548+poomport@users.noreply.github.com>
2024-03-06 15:51:47 +0700

    commit version 1.1.0

--- public/js/gotofunc.js
+++ public/js/gotofunc.js
@@ -1,34 +1,3 @@
-function openModal() {
-    // Display the modal
-    document.getElementById('modal0').style.display = 'block';
-}
-
-function closeModal() {
-    // Hide the modal
-    document.getElementById('modal0').style.display = 'none';
-}
-function openModal1() {
-    // Display the modal
-    document.getElementById('modal1').style.display = 'block';
-}
-
-function closeModal1() {
-    // Hide the modal
-    document.getElementById('modal1').style.display = 'none';
-}
-function openModal2() {
-    // Display the modal
-    document.getElementById('modal2').style.display = 'block';
-}
-
-function closeModal2() {
-    // Hide the modal
-    document.getElementById('modal2').style.display = 'none';
-}
-// Call openModal2() when the page loads
-document.addEventListener("DOMContentLoaded", function () {
-    openModal2();
-});
 function descript() {
 
     window.location.href = 'description.txt';
